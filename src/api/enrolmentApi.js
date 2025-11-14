3bb4aad52d9f0914b2d65906a8bbe677b991723b
Dr Adrian Clear <135222696+adrianclear@users.noreply.github.com>
2025-11-14 10:27:31 +0000

    updating url of the backend

--- src/api/enrolmentApi.js
+++ src/api/enrolmentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ct5106-full-stack-app-testing.onrender.com/students";
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/students`;
 
 export const getCourses = async (studentId) => {
   const res = await axios.get(`${BASE_URL}/${studentId}/enrolments`);
